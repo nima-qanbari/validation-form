a54608cc8ef709c79fc49d63ed31164cf6edae1f
nima <nimaqanbari1999@gmail.com>
2022-02-04 21:27:08 -0800

    add validate

--- src/Pages/SignUp.js
+++ src/Pages/SignUp.js
@@ -1,78 +1,86 @@
-import React, {useState} from 'react';
-import { Form,  } from 'react-bootstrap';
+import React, { useState, useEffect } from "react";
+import { Form, Button } from "react-bootstrap";
+
+import { validate } from "../components/Validate/validate";
+
 const SignUp = () => {
-   const [data, setData] = useState({
-       username: "",
-       email: "",
-       password: "",
-       confirmPassword: "",
+  const [data, setData] = useState({
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
 
-   })
+  const [errors, setErrors] = useState({})
 
+  useEffect(() => {
+      console.log(errors)
+    setErrors(validate(data))
+  }, [data])
 
-   const changeHandler = (e) => {
-       const {value, name} = e.target
-    setData({...data, [name]: value})
-   }
+  const changeHandler = (e) => {
+    const { value, name } = e.target;
+    setData({ ...data, [name]: value });
+  };
 
   return (
-  <div>
-
+    <div>
       <Form>
-          <h2>Create your account by filling out the information below</h2>
+        <h2>Create your account by filling out the information below</h2>
         <Form.Group>
-            <Form.Label>
-                Username
-            </Form.Label>
-            <Form.Control 
-            type='text'
-            placeholder='Username'
-            name='username'
+          <Form.Label>Username</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Username"
+            name="username"
             value={data.username}
             onChange={changeHandler}
-            />
+            
+          />
+          
         </Form.Group>
 
         <Form.Group>
-            <Form.Label>
-                Email
-            </Form.Label>
-            <Form.Control 
-            type='email'
-            placeholder='Email'
-            name='email'
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Email"
+            name="email"
             value={data.email}
             onChange={changeHandler}
-            />
+          />
         </Form.Group>
 
         <Form.Group>
-            <Form.Label>
-                Password
-            </Form.Label>
-            <Form.Control 
-            type='password'
-            placeholder='Password'
-            name='password'
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            name="password"
             value={data.password}
             onChange={changeHandler}
-            />
+          />
         </Form.Group>
 
         <Form.Group>
-            <Form.Label>
-                Confirm Password
-            </Form.Label>
-            <Form.Control 
-            type='password'
-            placeholder='Confirm Password'
-            name='confirmPassword'
+          <Form.Label>Confirm Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Confirm Password"
+            name="confirmPassword"
             value={data.confirmPassword}
             onChange={changeHandler}
-            />
+          />
+        </Form.Group>
+
+        <Form.Group>
+          <Button type="submit">Sign up</Button>
+          <p>
+            Already have an account? Login <a href="/">here</a>
+          </p>
         </Form.Group>
       </Form>
-  </div>
+    </div>
   );
 };
 
---
+++ src/components/Validate/validate.js
@@ -0,0 +1,45 @@
+
+export const validate = (data) => {
+
+let errors = {}
+
+//username
+if (!data.username.trim()) {
+    errors.username = "Username required"
+}else{
+    delete errors.username
+}
+
+
+//email
+if (!data.email){
+    errors.email = "Email required"
+}else if(!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(data.email)){
+    errors.email = "Invalid Email"
+}else {
+    delete errors.email
+}
+
+//password
+if(!data.password){
+    errors.password = "Password required"
+}else if (data.password.length < 8){
+    errors.password = "Your password must be 8 character or more"
+}else {
+    delete errors.password 
+}
+
+//confirm password
+
+if (!data.confirmPassword){
+    errors.confirmPassword = "Confirm the password "
+}else if (data.confirmPassword !== data.password){
+    errors.confirmPassword = "Password do not match"
+}else {
+    delete errors.confirmPassword
+}
+
+return errors
+};
+
+

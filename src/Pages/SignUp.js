4e047398ccc4ae11a4e99d1d6d1b5c99b2721ab4
nima <nimaqanbari1999@gmail.com>
2022-02-04 22:01:30 -0800

    add toched and toast

--- src/Pages/SignUp.js
+++ src/Pages/SignUp.js
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { Form, Button } from "react-bootstrap";
 
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { notify } from "../components/Toast/Toast";
+
 import { validate } from "../components/Validate/validate";
 
 const SignUp = () => {
@@ -12,20 +16,39 @@ const SignUp = () => {
   });
 
   const [errors, setErrors] = useState({})
+  const [touched, setTouched] = useState({})
 
   useEffect(() => {
-      console.log(errors)
     setErrors(validate(data))
-  }, [data])
+  }, [data, touched])
 
   const changeHandler = (e) => {
     const { value, name } = e.target;
     setData({ ...data, [name]: value });
   };
 
+  const tochedHandler = (e) => {
+    setTouched({...touched, [e.target.name]: true })
+  }
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+    if(!Object.keys(errors).length){
+        notify("You signed in successfully", "success")
+    }else{
+        notify("Invalid data", "error")
+        setTouched({
+            username:true,
+            email:true,
+            password:true,
+            confirmPassword:true,
+        })
+    }
+  }
+
   return (
     <div>
-      <Form>
+      <Form onSubmit={submitHandler}>
         <h2>Create your account by filling out the information below</h2>
         <Form.Group>
           <Form.Label>Username</Form.Label>
@@ -35,9 +58,9 @@ const SignUp = () => {
             name="username"
             value={data.username}
             onChange={changeHandler}
-            
+            onFocus={tochedHandler}
           />
-          
+          {errors.username && touched.username && <span>{errors.username}</span>}
         </Form.Group>
 
         <Form.Group>
@@ -48,7 +71,9 @@ const SignUp = () => {
             name="email"
             value={data.email}
             onChange={changeHandler}
+            onFocus={tochedHandler}
           />
+          {errors.email && touched.email && <span>{errors.email}</span>}
         </Form.Group>
 
         <Form.Group>
@@ -59,7 +84,9 @@ const SignUp = () => {
             name="password"
             value={data.password}
             onChange={changeHandler}
+            onFocus={tochedHandler}
           />
+          {errors.password && touched.password && <span>{errors.password}</span>}
         </Form.Group>
 
         <Form.Group>
@@ -70,7 +97,9 @@ const SignUp = () => {
             name="confirmPassword"
             value={data.confirmPassword}
             onChange={changeHandler}
+            onFocus={tochedHandler}
           />
+          {errors.confirmPassword && touched.confirmPassword && <span>{errors.confirmPassword}</span>}
         </Form.Group>
 
         <Form.Group>
@@ -79,6 +108,7 @@ const SignUp = () => {
             Already have an account? Login <a href="/">here</a>
           </p>
         </Form.Group>
+        <ToastContainer />
       </Form>
     </div>
   );
